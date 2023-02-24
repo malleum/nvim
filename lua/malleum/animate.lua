6096a95686820d4abe4fc82779a8279fdc4d41e7
Joshua Hammer <jph33@outlook.com>
2023-02-24 17:42:47 -0500

    added down anami

--- lua/malleum/animate.lua
+++ lua/malleum/animate.lua
@@ -1,15 +1,43 @@
 local config = {
-    fps = 40,
+    fps = 50,
     name = 'slide',
 }
+
 config.update = function (grid)
     for i = 1, #grid do
-        local prev = grid[i][#(grid[i])]
+        local last_char = grid[i][#(grid[i])]
         for j = 1, #(grid[i]) do
-            grid[i][j], prev = prev, grid[i][j]
+            grid[i][j], last_char = last_char, grid[i][j]
         end
     end
     return true
 end
 
 require("cellular-automaton").register_animation(config)
+
+local config2 = {
+    fps = 30,
+    name = 'down',
+}
+
+config2.update = function (grid)
+    local largest = 0
+    local ind = 0
+    for i = 1, #grid do
+        local size = #grid[i]
+        if size > largest then
+            largest = size
+            ind = i
+        end
+    end
+
+    for j = 1, #grid[ind] do
+        local bottom_char = grid[#grid][j]
+        for i = 1, #grid do
+            grid[i][j], bottom_char = bottom_char, grid[i][j]
+        end
+    end
+    return true
+end
+
+require("cellular-automaton").register_animation(config2)
