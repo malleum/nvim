5fa9271ffb0b57af763c54f701e638f216cb5861
Joshua Hammer <jph33@outlook.com>
2023-03-09 16:04:08 -0500

    added neovide settings

---
+++ lua/malleum/neovide.lua
@@ -0,0 +1,13 @@
+
+local g = vim.g
+
+if g.neovide then
+    g.neovide_hide_mouse_when_typing = true
+    g.neovide_refresh_rate = 60
+    g.neovide_fullscreen = true
+    g.neovide_cursor_vfx_mode = "railgun"
+    g.neovide_cursor_vfx_particle_desity = 20
+    g.neovide_cursor_vfx_particle_speed = 27
+    g.neovide_cursor_vfx_particle_phase = 0.7
+    g.neovide_cursor_vfx_particle_curl = 0.7
+end
