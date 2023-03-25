a7642b7e62334584fd075f461e4788799e94f3d0
Joshua Hammer <jph33@outlook.com>
2023-03-24 23:27:09 -0400

    transparent update

--- lua/malleum/settings.lua
+++ lua/malleum/settings.lua
@@ -49,15 +49,20 @@ g.better_whitespace_guicolor = "darkblue"
 
 -- Colorscheme
 require("transparent").setup({
-    enable = true,
-    extra_groups = {
-        "BufferLineTabClose",
-        "BufferlineBufferSelected",
-        "BufferLineFill",
-        "BufferLineBackground",
-        "BufferLineSeparator",
-        "BufferLineIndicatorSelected",
+    groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
     },
+    -- extra_groups = {
+    --     "BufferLineTabClose",
+    --     "BufferlineBufferSelected",
+    --     "BufferLineFill",
+    --     "BufferLineBackground",
+    --     "BufferLineSeparator",
+    --     "BufferLineIndicatorSelected",
+    -- },
 })
 
 vim.cmd.colorscheme('OceanicNext')
