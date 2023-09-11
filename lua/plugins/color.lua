f399e2bfa06b7b89ff1ba8e0c8da20dff07e8792
Joshua Hammer <jph33@outlook.com>
2023-09-11 10:45:32 -0400

    removed unused color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -3,19 +3,14 @@ return {
         'mhartington/oceanic-next',
         lazy = false,
         priority = 1000,
+        dependencies = {
+            'xiyaowong/nvim-transparent'
+        },
         config = function()
             vim.cmd.colorscheme('OceanicNext')
             vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
             vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
         end
-    },
-    {
-        'folke/tokyonight.nvim',
-        event = "VeryLazy",
-    },
-    {
-        'EdenEast/nightfox.nvim',
-        event = "VeryLazy",
     },
     {
         'ntpeters/vim-better-whitespace',
@@ -26,8 +21,5 @@ return {
             vim.g.strip_whitespace_confirm = 0
             vim.g.better_whitespace_guicolor = "darkblue"
         end
-    },
-    {
-        'xiyaowong/nvim-transparent',
     },
 }
