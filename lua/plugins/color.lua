c680212e90393e3e2edc5707240ce92fb8d9a611
Joshua Hammer <jph33@outlook.com>
2023-12-05 21:02:08 -0500

    colorscheme change

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,13 +1,22 @@
+local color = "folke/tokyonight.nvim"
+-- local color = "mhartington/oceanic-next"
+
 return {
     {
-        'mhartington/oceanic-next',
+        color,
         lazy = false,
         priority = 1000,
         dependencies = { 'xiyaowong/nvim-transparent' },
         config = function()
-            vim.cmd.colorscheme('OceanicNext')
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            if color == "mhartington/oceanic-next" then
+                vim.cmd.colorscheme([[OceanicNext]])
+                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+                vim.cmd.TransparentEnable()
+            else
+                vim.cmd.TransparentDisable()
+                vim.cmd.colorscheme([[tokyonight-night]])
+            end
         end
     },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,16 +4,13 @@ return {
     event = "VeryLazy",
     config = function()
         require('lualine').setup {
-            options = {
-                color = { fg = '#000000' },
-            },
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
                 lualine_x = { 'searchcount', 'filetype', 'filesize' },
                 lualine_y = { 'location' },
-                lualine_z = { function() return os.time() end, function() return os.date("%X") end }
+                lualine_z = { function() return os.time() end}
             },
         }
     end
