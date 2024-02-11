d381fc7d7b664ad3f6240d2849a89faf4a101c05
Joshua Hammer <jph33@outlook.com>
2024-02-10 20:59:16 -0500

    rm transparent.nvim

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -3,10 +3,8 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        dependencies = {
-            "xiyaowong/transparent.nvim"
-        },
         config = function()
+            require("tokyonight").setup({ transparent = true })
             vim.cmd.colorscheme([[tokyonight-night]])
         end
     },
