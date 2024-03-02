c692cbfb4522c74cd3b7d5f216e016129077cf74
Joshua Hammer <jph33@outlook.com>
2024-03-02 12:32:37 -0500

    flash

--- lua/plugins/zoom.lua
+++ lua/plugins/zoom.lua
@@ -1,20 +1,22 @@
 return {
 	{ "michaeljsmith/vim-indent-object", event = "VeryLazy" },
 	{
-		"ggandor/leap-spooky.nvim",
+		"folke/flash.nvim",
 		event = "VeryLazy",
-		dependencies = {
-			"ggandor/flit.nvim",
-			"tpope/vim-repeat",
-			"ggandor/leap.nvim",
+		opts = {
+			label = { rainbow = { enabled = true } },
+			modes = {
+				search = { enabled = false },
+				char = { keys = { "f", "F", "t", "T", [";"] = "<cr>", [","] = "<tab>" } },
+				remote_op = { restore = true, motion = true },
+			},
 		},
-		config = function()
-			require("leap").add_default_mappings()
-			vim.keymap.del({ "x", "o" }, "x")
-			vim.keymap.del({ "x", "o" }, "X")
-			require("flit").setup({})
-
-			require("leap-spooky").setup({})
-		end,
+        -- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
+			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		},
 	},
 }
