5d0973f2b6a2f7afa80c7ddcd6017197b55edb03
Joshua Hammer <jph33@outlook.com>
2024-03-07 15:34:00 -0500

    Leap and telekinesis is back

--- lua/plugins/zoom.lua
+++ lua/plugins/zoom.lua
@@ -1,18 +1,15 @@
 return {
 	{ "michaeljsmith/vim-indent-object", event = "VeryLazy" },
 	{
-		"folke/flash.nvim",
+		"ggandor/leap-spooky.nvim",
 		event = "VeryLazy",
-		opts = {
-			label = { rainbow = { enabled = true } },
-			modes = { search = { enabled = false } },
-		},
-        -- stylua: ignore
-		keys = {
-			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-		},
+		dependencies = { "ggandor/leap.nvim" },
+		config = function()
+			require("leap").add_default_mappings()
+			vim.keymap.del({ "x", "o" }, "x")
+			vim.keymap.del({ "x", "o" }, "X")
+
+			require("leap-spooky").setup()
+		end,
 	},
 }
