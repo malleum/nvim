8f16abe0d721254aa60463271a63ce2eba40d1bd
Joshua Hammer <jph33@outlook.com>
2024-04-02 15:42:27 -0400

    flash is back

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,23 +10,17 @@ return {
 			harpoon:list():select(num)
 		end
 
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
-		end)
-		vim.keymap.set("n", "<leader>o", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-		vim.keymap.set("n", "<C-A-h>", function()
-			hrpnslct(1)
-		end)
-		vim.keymap.set("n", "<C-A-t>", function()
-			hrpnslct(2)
-		end)
-		vim.keymap.set("n", "<C-A-n>", function()
-			hrpnslct(3)
-		end)
-		vim.keymap.set("n", "<C-A-s>", function()
-			hrpnslct(4)
-		end)
+        -- stylua: ignore
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        -- stylua: ignore
+		vim.keymap.set("n", "<leader>o", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        -- stylua: ignore
+		vim.keymap.set("n", "<C-A-h>", function() hrpnslct(1) end)
+        -- stylua: ignore
+		vim.keymap.set("n", "<C-A-t>", function() hrpnslct(2) end)
+        -- stylua: ignore
+		vim.keymap.set("n", "<C-A-n>", function() hrpnslct(3) end)
+        -- stylua: ignore
+		vim.keymap.set("n", "<C-A-s>", function() hrpnslct(4) end)
 	end,
 }
